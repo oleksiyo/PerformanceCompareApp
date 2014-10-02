a167b40f30cc27b91a3556793f06ff886b903261
Aleksey Ozemko <aleksey.ozemko@sigmaukraine.com>
2014-10-02 11:54:10 +0300

    created project

---
+++ CSharpApp/PersonGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpApp
+{
+    public class PersonGenerator
+    {
+        public List<Person> GetPersons(int count)
+        {
+            return Enumerable.Range(0, count).Select(CreatePerson).ToList();
+        }
+
+        private Person CreatePerson(int personId)
+        {
+            var random = new Random();
+            var personalNumber = String.Empty;
+
+                for (var i = 0; i < 900; i++)
+                {
+                    personalNumber += random.Next(0, 9).ToString() + "Number";
+                }
+
+            return new Person
+                {
+                    Id = personId,
+                    Amount = random.Next(1, 10),
+                    Earned = 0,
+                    PersonalNumber = personalNumber,
+                    RegisteredDate = DateTime.Now,
+                    Salary = random.Next(100, 1000),
+                    Signature = "Signature"
+                    
+                };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string PersonalNumber { get; set; }
+        public int Amount { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public string Signature { get; set; }
+        public int Salary { get; set; }
+        public int Earned { get; set; }
+    }
+}
---
+++ PerformanceCompareApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PerformanceCompareApp
+{
+    class Program
+    {
+        private const int count = 10000;
+        static void Main(string[] args)
+        {
+            TestFsharp();
+            TestCsharp();
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+
+        private static void TestFsharp()
+        {
+            var generator = new FSharpApp.PersonGenerator();
+            var stopWatch = new Stopwatch();
+            
+            stopWatch.Start();
+            var persons = generator.GetPersons(count);
+            stopWatch.Stop();
+
+            Console.WriteLine("F#: " + stopWatch.Elapsed.TotalSeconds.ToString() + " seconds");
+        }
+
+        private static void TestCsharp()
+        {
+            var generator = new CSharpApp.PersonGenerator();
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var persons = generator.GetPersons(count);
+            stopWatch.Stop();
+
+            Console.WriteLine("C#: " + stopWatch.Elapsed.TotalSeconds.ToString() + " seconds");
+        }
+    }
+}
